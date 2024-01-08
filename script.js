2268282f9b0da98d29bc3349cd0a04356fff2eba
EricaMarzi <erica.marzi.1993@gmail.com>
2024-01-08 17:02:04 +0100

    print in console

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ Ricordiamoci di inizializzare la variabile di contatore prima dell'inizio del ci
 Ricordiamoci di incrementare il contatore, o comunque di inserire una condizione di uscita, all'interno ciclo while
 Se abbiamo difficoltà con lo stampare in pagina, proviamo prima a stampare col ciclo for e solo dopo convertiamo il for in while
 */
+
+
+const shoppingList = ["latte", "uova", "pesce", "biscotti", "pane", "tonno", "prosciutto", "pasta"]
+
+for (let i = 0 ; i < shoppingList.length; i++) {
+    console.log (shoppingList[i])
+}
+
+/*let i = 0
+while (i < shoppingList.length) {
+    console.log(shoppingList[i])
+    i++
+}
+*/
